3ee3bbb42f0f6a1ed0858a6b759e21ae90cc55c6
Toey <toeylui.dev@gmail.com>
2025-05-02 23:20:16 -0700

    Add AI bot

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Widgets from './Widgets.jsx';
+import Chatbot from '../components/Chatbot.jsx';
 
 const HomePage = () => {
   return (
       <>
         <title>Home</title>
         <Widgets />
+        <Chatbot />
       </>
   );
 };
