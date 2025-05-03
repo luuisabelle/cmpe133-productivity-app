985e0d79c9a0053a2edb4b3326e2ad4210fd34e0
tofu <isabelleluu0w0@gmail.com>
2025-05-02 23:30:52 -0700

    env stuff

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './contexts/AuthContext.jsx';
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import dotenv from "dotenv";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthProvider>
-      <GoogleOAuthProvider clientId='1019470647576-v8k9cdftm9u3emjputkcc4kpm8abrgnt.apps.googleusercontent.com'>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <App />
     </GoogleOAuthProvider>
     </AuthProvider>
